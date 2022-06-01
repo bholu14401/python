8deb6294d5c57086d2ebbee31ca2f4f75e1e702a
bholu14401 <74172023+bholu14401@users.noreply.github.com>
2022-06-01 15:28:38 +0530

    Create PermutationInString.py

---
+++ PermutationInString.py
@@ -0,0 +1,32 @@
+"""Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+Example 1:
+
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+
+"""
+
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        counts = collections.Counter(s1)
+        l = len(s1)
+        for i in range(len(s2)):
+            if counts[s2[i]] > 0:
+                l -= 1
+            counts[s2[i]] -= 1
+            if l == 0:
+                return True
+            start = i + 1 - len(s1)
+            if start >= 0:
+                counts[s2[start]] += 1
+                if counts[s2[start]] > 0:
+                    l += 1
+        return False
